a5a764f3c0e8432ed596498b101a9ad368c12315
sumitramkr <sumitramkr77e@gmail.com>
2022-06-29 11:18:20 +0530

    Initialize project using Create React App

---
+++ src/components/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function App() {
+  const [list, setList] = React.useState("");
+  const [arrayList, setArrayList] = React.useState([]);
+
+  function handleChange(event) {
+    const values = event.target.value;
+    setList(values);
+  }
+
+  function handleClick() {
+    setArrayList((arrayList) => [...arrayList, list]);
+    setList("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={handleChange} value={list} />
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {arrayList.map((items) => (
+            <li>{items}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
